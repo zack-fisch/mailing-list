24c9662c152654659b7c60255a629f99e2e69afe
Zack Fisch <zack@zfisch.com>
2014-09-03 20:44:16 -0400

    Basic playing around

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,10 @@
 class VisitorsController < ApplicationController
 
  def new
+ 	Rails.logger.debug 'DEBUG: entering new method'
  	@owner = Owner.new
+ 	Rails.logger.debug 'DEBUG:: Owner name is ' + @owner.name
+
  end
 
 end
