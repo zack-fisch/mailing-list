aaed308ff84c03e01d87efe69945c7a71994d572
Zack Fisch <zack@zfisch.com>
2014-09-10 15:56:42 -0400

    Contact controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,20 @@
 class ContactsController < ApplicationController
-  def process_form
-  	Rails.logger.debug "DEBUG: params are #{params}"
-  	flash[:notice] = "Recieved request from #{params[:contact][:name]}"
-  	redirect_to root_path
-  end
+ def new
+ 	@contact = Contact.new
+ end
+
+ def create
+ 	if @contact.valid?
+ 		@contact.save
+ 	end
+ 	flash[:notice] = "Message sent from #{@contact.name}."
+ 	redirect_to root_path
+ end
+
+ private
+
+ def secure_params
+ 	params.require(:contact).permit(:name, :email, :content)
+ end
+
 end
